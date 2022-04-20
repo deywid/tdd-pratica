176011ec1dbd1e76a257be0bbdec478d9d49e3a9
deywid <deywid.felipe@gmail.com>
2022-04-20 13:30:21 -0300

    Adicionar arquivos de projeto.

---
+++ tdd-pratica.UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tdd_pratica.UnitTest
+{
+    [TestClass]
+    public class GirarListaTest
+    {
+        [TestMethod]
+        public void DeveriaMoverPrimeiroItemParaOFinal()
+        {
+            var lista = new int[] { 1, 2, 3, 4 };
+            var gira = new GiraLista();
+
+            var nova = gira.Girar(lista);
+
+            Assert.AreEqual(lista.Length, nova.Length);
+            Assert.AreEqual(1, nova[3]);
+        }
+    }
+}
